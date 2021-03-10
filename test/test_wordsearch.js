407baefba8bd504ff007907f6d4fed51ae814ed4
Paul Ladd <vlendle@gmail.com>
2021-03-10 23:08:45 +0000

    Implemented horizontal and vertical forwards and backwards searching

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,55 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'R', 'N', 'F', 'E', 'L', 'D'],
+      ['S', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['E', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['R', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['T', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ASSERT')
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present backwards horizontally", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'E', 'R', 'N', 'F', 'E', 'L', 'D'],
+      ['S', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['F', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['R', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['T', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present backwards vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'E', 'R', 'N', 'F', 'E', 'L', 'D'],
+      ['S', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['F', 'M', 'J', 'T', 'E', 'V', 'R', 'E'],
+      ['R', 'H', 'C', 'S', 'Y', 'E', 'R', 'F'],
+      ['T', 'F', 'R', 'E', 'N', 'E', 'Y', 'N'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'E'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'S'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+  it("should return true 'smile' if the array is empty", function() {
+    const result = wordSearch([], 'smile')
+
+    assert.equal(result, 'smile');
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,45 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// Pair programming with @code-driving and @itspladd!
+
+const transpose = function(matrix) {
+  // Put your solution here
+  const result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0) {
+        result.push([matrix[i][j]]);
+      } else {
+        result[j].push(matrix[i][j]);
+      }
+
+
     }
-}
+  }
+  return result;
+};
+
+// Searches forwards and backwards, horizontally and vertically. Does not search diagonally
+
+const wordSearch = (letters, word) => { 
+  if (!letters.length) {
+      return `smile`;
+  }
+  const reversedWord = word.split("").reverse().join("");
+  const transposed = transpose(letters);
+  const horizontalJoin = letters.map(chars => chars.join(''));
+  const verticalJoin = transposed.map(chars => chars.join(''));
+
+  for (row of horizontalJoin) {
+      if (row.includes(word)) return true;
+      if (row.includes(reversedWord)) return true;
+  }
+  for (column of verticalJoin) {
+    if (column.includes(word)) return true;
+    if (column.includes(reversedWord)) return true;
+  }
+
+  return false;
+}; 
+
 
 module.exports = wordSearch
